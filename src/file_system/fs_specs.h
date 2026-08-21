5c931ef8500e93d2c3dc30446bf4d3f7e5649bac
xanderec <>
2026-08-21 19:21:00 -0400

    [2/n][filesys] add specs header file for constants

---
+++ src/file_system/fs_specs.h
@@ -0,0 +1,17 @@
+#define BLOCK_SIZE 2048 // 2kB
+#define NUM_BLOCKS 64
+
+#define INODE_SIZE 128 // 128B
+#define NUM_INODES 80 // 16 inodes/block across 5 blocks
+#define ROOT_INODE 0
+
+// file system disk layout
+#define SUPERLOCK_BLOCK 0
+#define INODE_BITMAP_BLOCK 1
+#define DATA_BITMAP_BLOCK 2
+#define INODE_TABLE_START 3
+#define INODE_TABLE_BLOCKS 5
+#define DATA_REGION_START 8
+
+#define MAX_FILENAME_LEN 60
+#define MAX_EXTENTS 4
