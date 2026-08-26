339d6fca2f9d7c20548f97d67eab7c81e00fb730
xanderec <>
2026-08-26 16:36:34 -0400

    [27/n][filesys] add read & write primitives to data block layer

--- src/file_system/data_block.h
+++ src/file_system/data_block.h
@@ -1,2 +1,6 @@
+#include "block_layer.h"
+
 int data_block_alloc();
 int data_block_free(int data_block);
+int data_block_read(block* out, int data_block);
+int data_block_write(block* in, int data_block);
