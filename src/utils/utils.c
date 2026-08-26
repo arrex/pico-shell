6b3b1d979b5ae5f860d5158fd159dea219b30b7e
xanderec <>
2026-08-26 16:31:22 -0400

    [12/n][filesys] add dir_add primitive to directory layer

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 /*
-  Checks if a string is valid. Used for checking file and directory names.
-  Alphanumeric characters and underscores are allowed.
-  Returns:
-  - 0 if not alphanumeric
-  - 1 if alphanumeric
+ * Checks if a string is valid. Used for checking file and directory names.
+ * Alphanumeric characters and underscores are allowed.
+ *
+ * Returns:
+ * - 0 if not alphanumeric
+ * - 1 if alphanumeric
 */
 int is_valid_name(char* string) {
     if (string == NULL) {
@@ -23,4 +24,15 @@ int is_valid_name(char* string) {
     }
 
     return 1;
+}
+
+/*
+ * performs ceiling division using formula:
+ *
+ * ceil(n / k) = (n + k - 1) / k
+ *
+ * returns an integer, hence the suffix `i`.
+ */
+int ceili(int n, int k) {
+    return (n + k - 1) / k;
 }
