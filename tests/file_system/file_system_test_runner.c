abf743ac75f7ef9635763aa1441e6fd9ec94e384
xanderec <>
2026-08-26 16:31:22 -0400

    [25/n][test] add unit tests for inode layer

--- tests/file_system/file_system_test_runner.c
+++ tests/file_system/file_system_test_runner.c
@@ -1,6 +1,7 @@
 #include "../../src/file_system/disk.h"
 #include "block_layer_test.h"
 #include "disk_test.h"
+#include "inode_test.h"
 #include "unity_internals.h"
 
 void setUp(void) { disk_reset(); }
@@ -25,6 +26,21 @@ int main(void) {
     RUN_TEST(test_block_writes_are_independent);
     RUN_TEST(test_invalid_read_inputs_preserve_state);
     RUN_TEST(test_invalid_write_inputs_preserve_state);
+
+    // inode layer tests
+    RUN_TEST(test_inode_alloc_allocates_first_free_slot);
+    RUN_TEST(test_inode_alloc_uses_lowest_available_slot);
+    RUN_TEST(test_inode_alloc_preserves_existing_inode_entries);
+    RUN_TEST(test_inode_alloc_crosses_inode_table_block_boundary);
+    RUN_TEST(test_inode_alloc_returns_failure_when_inode_table_is_full);
+    RUN_TEST(test_inode_alloc_invalid_inputs);
+    RUN_TEST(test_inode_free_marks_inode_available);
+    RUN_TEST(test_inode_free_fails_when_slot_already_free);
+    RUN_TEST(test_inode_free_invalid_inputs);
+    RUN_TEST(test_inode_read_reads_inode_at_requested_slot);
+    RUN_TEST(test_inode_read_invalid_inputs);
+    RUN_TEST(test_inode_write_updates_only_requested_inode);
+    RUN_TEST(test_inode_write_invalid_inputs);
 
     return UNITY_END();
 }
