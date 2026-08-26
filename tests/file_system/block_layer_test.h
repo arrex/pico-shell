432e48e50d959fbe5024bbcd9c3ec9b86a1f5a17
xanderec <>
2026-08-26 16:33:32 -0400

    [18/n][test] add unit tests for block layer

---
+++ tests/file_system/block_layer_test.h
@@ -0,0 +1,6 @@
+void test_block_starts_as_zeroes(void);
+void test_block_write_read_round_trip(void);
+void test_block_write_only_changes_target_block(void);
+void test_block_writes_are_independent(void);
+void test_invalid_read_inputs_preserve_state(void);
+void test_invalid_write_inputs_preserve_state(void);
