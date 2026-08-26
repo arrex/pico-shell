db10b8f1ee7458da14dfbd943523e43c3dcbcc1d
xanderec <>
2026-08-26 16:37:19 -0400

    [8/n][refactor] update filesys naming conventions

--- src/file_system/data_block.h
+++ src/file_system/data_block.h
@@ -1,9 +1,10 @@
 #include "fs_specs.h"
+#include "block_layer.h"
 
 typedef struct dir_entry {
     char filename[MAX_FILENAME_LEN];
     int inode;
 } dir_entry;
 
-int data_block_alloc(struct block* buf);
-int data_block_free(int block_num);
+int data_block_alloc();
+int data_block_free(int data_block);
