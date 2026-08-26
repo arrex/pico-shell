8a0ad09fafd76b81295fdfb77203019b328fdbbc
xanderec <>
2026-08-26 16:45:50 -0400

    [16/n][test] add unit tests for disk layer

--- src/file_system/disk.h
+++ src/file_system/disk.h
@@ -1,2 +1,13 @@
+#pragma once
+
+#include "file_system.h"
+
+#define DISK_SIZE (NUM_BLOCKS * BLOCK_SIZE)
+
 int disk_read(void* buf, int size, int offset);
 int disk_write(const void* buf, int size, int offset);
+
+// functions exposed for unit testing orchestration
+#ifdef PICOSHELL_TESTING
+void disk_reset(void);
+#endif
