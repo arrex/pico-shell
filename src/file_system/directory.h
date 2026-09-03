b4f5051a73480d4e802fe55156527568d4c8fb00
arrex <>
2026-09-03 17:03:31 -0400

    [3/n][filesys] update directory layer lookup primitive

--- src/file_system/directory.h
+++ src/file_system/directory.h
@@ -11,6 +11,6 @@ typedef struct dirent {
 // forward decls
 struct inode;
 
-bool dir_lookup(int inode_num, const char* filename);
+int dir_lookup(int inode_num, const char* filename);
 int dir_add(int inode_num, struct dirent* new_dirent);
 int dir_remove(int inode_num, const char* filename);
