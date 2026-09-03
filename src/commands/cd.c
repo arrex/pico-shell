1ab72168c352eabd2d001e14e57f95f571f744e3
arrex <>
2026-09-03 17:48:04 -0400

    [7/n][cmds] hook mkdir into file system

--- src/commands/cd.c
+++ src/commands/cd.c
@@ -4,8 +4,8 @@
 #include "../utils/utils.h"
 
 int cd(char* dirname) {
-    if (is_valid_name(dirname) != 1) {
-        return badcommandNameNotAlphanum();
+    if (is_valid_path(dirname) != 1) {
+        return badcommandNotAlphanum();
     }
 
     if (chdir(dirname) != 0) {
